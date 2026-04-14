0736094ce69d5e3e8ac0e219e90fe561770d38e0
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2026-04-14 19:31:31 +0200

    Update settings.kts: add requirements

--- .teamcity_update_to_default_dsl/settings.kts
+++ .teamcity_update_to_default_dsl/settings.kts
@@ -77,6 +77,16 @@ object CrudControllerPipeline_Job1 : Job({
             scriptContent = """echo "CRUD""""
         }
     }
+
+    requirements {
+            startsWith("system.agent.name", "Default", "RQ_1")
+            doesNotContain("system.agent.name", "Windows", "RQ_2")
+            endsWith("system.agent.name", "Agent", "RQ_3")
+            doesNotMatch("system.agent.name", "ip", "RQ_ip")
+
+            moreThanVer("docker.version", "27.1", "docker")
+            lessThanVer("DotNetCLI", "10", "dotnet")
+        }
 })
 
 
